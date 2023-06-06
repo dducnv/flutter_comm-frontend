539d8ad6962956ed863ba5881ff5162e78f3d3b6
Nguyễn Văn Đức <ducbe2k2@gmail.com>
2023-06-06 22:26:49 +0700

    home page update

--- untils/configs/app_api_config.ts
+++ untils/configs/app_api_config.ts
@@ -6,6 +6,7 @@ export const appApi = {
     const access_token = token ? `Bearer ${token}` : "";
     return await fetch(url, {
       method: "GET",
+      cache: "no-cache",
       next: {
         ...next,
       },
